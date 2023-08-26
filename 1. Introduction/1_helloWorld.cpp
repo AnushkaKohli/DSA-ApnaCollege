9a87a6ff268c50a73b80887b8d6f122234ffcefe
AnushkaKohli <102284552+AnushkaKohli@users.noreply.github.com>
2023-08-26 09:13:35 +0530

    introduction done

---
+++ 1. Introduction/1_helloWorld.cpp
@@ -0,0 +1,13 @@
+#include <iostream>
+#include <cstdlib>
+using namespace std;
+
+int main()
+{
+    int a, b;
+    cout << "Hello World!" << endl;
+    cout << "Enter two numbers to add: " << endl;
+    cin >> a >> b;
+    cout << "The sum is: " << a + b << endl;
+    return EXIT_SUCCESS;
+}
---
+++ 2. Data Types and Loops/1_dataTypes.cpp
@@ -0,0 +1,34 @@
+#include <iostream>
+using namespace std;
+
+int main()
+{
+    int a;
+    cout << "Size of int: " << sizeof(a) << endl;
+
+    float b;
+    cout << "Size of float: " << sizeof(b) << endl;
+
+    char c;
+    cout << "Size of char: " << sizeof(c) << endl;
+
+    bool d;
+    cout << "Size of bool: " << sizeof(d) << endl;
+
+    short int e;
+    cout << "Size of short int: " << sizeof(e) << endl;
+
+    long int f;
+    cout << "Size of long int: " << sizeof(f) << endl;
+
+    long long int g;
+    cout << "Size of long long int: " << sizeof(g) << endl;
+
+    double h;
+    cout << "Size of double: " << sizeof(h) << endl;
+
+    long double i;
+    cout << "Size of long double: " << sizeof(i) << endl;
+
+    return 0;
+}
