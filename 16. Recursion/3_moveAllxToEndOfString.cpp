8442fd6fcb8e229419ff89f51251e21ea1df0bff
AnushkaKohli <102284552+AnushkaKohli@users.noreply.github.com>
2023-09-29 23:33:16 +0530

    move all x to end of string using recursion

--- 16. Recursion/3_moveAllxToEndOfString.cpp
+++ 16. Recursion/3_moveAllxToEndOfString.cpp
@@ -1,18 +1,18 @@
-// "aaaabbbeeecdddd" -> "abecd"
+// "axxbdxcefxhix" -> "abdcefhixxxxx"
 
 #include <iostream>
 using namespace std;
 
 // Time complexity: O(n^2) because s.substr(i) takes O(n) time and it is called n times.
 // Space complexity: O(n^2) because s.substr(i) gives a string of O(n) size and is called n times.
-string removeAllDuplicates(string s)
+string moveAllx(string s)
 {
     if (s.length() == 0)
         return "";
     char ch = s[0];
-    string ans = removeAllDuplicates(s.substr(1));
-    if (ch == ans[0])
-        return ans;
+    string ans = moveAllx(s.substr(1));
+    if (ch == 'x')
+        return (ans + ch);
     else
         return (ch + ans);
 }
@@ -22,6 +22,6 @@ int main()
     string s;
     cout << "Enter the string: ";
     cin >> s;
-    cout << "String after removing all duplicates: " << removeAllDuplicates(s) << "\n";
+    cout << "String after moving all x's to end: " << moveAllx(s) << "\n";
     return 0;
 }
