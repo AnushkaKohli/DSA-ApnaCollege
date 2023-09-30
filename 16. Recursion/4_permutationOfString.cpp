69e2a0e0ff0b6fe3d0bb3764febf3daffa7f653e
AnushkaKohli <102284552+AnushkaKohli@users.noreply.github.com>
2023-09-30 12:44:00 +0530

    permutation of string

---
+++ 16. Recursion/4_permutationOfString.cpp
@@ -0,0 +1,37 @@
+// Print all possible permuattions of a string
+//                 ABC
+//      /           |          \
+//    BC,A         AC,B       AB,C
+//   /    \       /    \     /    \
+// C,AB  B,AC  C,BA  A,BC  B,CA  A,CB
+//  |      |     |     |     |     |
+// ABC    ACB   BAC   BCA   CAB   CBA
+
+#include <iostream>
+using namespace std;
+
+void permutation(string s, string ans)
+{
+    if (s.length() == 0)
+    {
+        cout << ans << endl;
+        return;
+    }
+    // i is the index of character to be fixed in the string
+    for (int i = 0; i < s.length(); i++)
+    {
+        char charToBeFixed = s[i];
+        string restOfString = s.substr(0, i) + s.substr(i + 1);
+        permutation(restOfString, ans + charToBeFixed);
+    }
+}
+
+int main()
+{
+    string s;
+    cout << "Enter the string: ";
+    cin >> s;
+    cout << "The possible permutations are: " << endl;
+    permutation(s, "");
+    return 0;
+}
